c2e6ea981d56daa80515a08827ca2ce2bd38282a
Kayque-Maciel <kayquemaciel000@gmail.com>
2023-08-07 21:11:43 -0300

    Finalizado

---
+++ Loops/script.js
@@ -0,0 +1,28 @@
+/*
+
+//WHILE = enquanto
+
+var x = 5;
+
+while(x < 10){
+    document.write("<br> O valor do x é : " + x);
+
+    x++;
+}
+
+*/
+
+/*
+// FOR = para
+
+var valor = 30;
+
+for(a = 0; a <= valor; a++){
+    //o que tiver aqui dentro
+
+    document.write("<br> Valor do A é: " + a);
+}
+
+*/
+
+
